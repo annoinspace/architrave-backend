692d5574fd1036a81e13262d2471209291934995
annoinspace <aneesahalmask@gmail.com>
2023-03-01 12:56:21 +0000

    updates to the color sorter

--- src/api/lib/colorSorter.js
+++ src/api/lib/colorSorter.js
@@ -1,47 +1,78 @@
 export const getCategoryFromRGB = (rgb) => {
   const [r, g, b] = rgb
   console.log("rgb before switch cases", { r, g, b })
+  // Find the highest and smallest RGB values
+  const highestValue = Math.max(r, g, b)
+  const smallestValue = Math.min(r, g, b)
 
   switch (true) {
     case r === 255 && g === 255 && b === 255:
       return "white"
+    case r >= 240 && g >= 240 && b >= 240:
+      return "white"
+    case r === 0 && g === 0 && b === 0:
+      return "gray"
+    case r === g && r === b && g === b:
+      return "gray"
+    case r === g && r === b && g === b:
+      return "gray"
     case r >= 150 && g <= 100 && b <= 100:
       return "red"
     case r >= 150 && r - g > 129 && b <= 100:
       return "red"
+    case r > g && g === b && b >= 40:
+      return "red"
+    case r === 0 && g === 0 && b >= 50:
+      return "blue"
     case r <= 100 && g <= 100 && b >= 150:
       return "blue"
-    case r === 0 && g === b:
+    case r >= 0 && g - r >= 15 && b === highestValue:
+      return "blue"
+    case r === 0 && g === b && b > 0:
+      return "blue"
+    case r === smallestValue && g === b && b > 0:
       return "blue"
     case r < g && g >= 150 && b <= 100:
       return "green"
+    case r < b && g >= 200 && b >= 100:
+      return "green"
     case r <= 100 && g >= 150 && b < g:
+      return "green"
+    case r === b && g === highestValue && b > 0:
+      return "green"
+    case r === g && b === smallestValue && b > 0:
+      return "green"
+    case g === highestValue && b === smallestValue && b <= 50 && r > 0:
       return "green"
     case r >= 200 && g <= 150 && b >= 150:
       return "pink"
-    case r >= 200 && g >= 120 && b >= 120:
+    case r >= 245 && g < b && b >= 80:
       return "pink"
+    case r === b && g === 0 && b > 0:
+      return "pink"
+    case r < 50 && g < 50 && b >= 50 && b <= 120:
+      return "purple"
     case r >= 150 && g <= 100 && b >= 150:
       return "purple"
-    case r === b && g === 0:
+    case b - r > 10 && r - g >= 5 && b >= 50:
       return "purple"
-    case r >= 200 && g >= 200 && b <= 100:
-      return "yellow"
     case r <= 200 && g === r && r - b >= 40:
       return "yellow"
-    case r === g && b === 0:
+    case r >= 180 && r - g <= 15 && b < g:
+      return "yellow"
+    case r === g && b === 0 && r > 0:
       return "yellow"
     case r >= 200 && g >= 100 && b <= 100:
       return "orange"
     case r >= 150 && g >= 120 && b <= 100:
       return "orange"
+    case r >= 150 && g >= 120 && g - b >= 20:
+      return "orange"
     case r >= 100 && g <= 50 && b <= 50:
       return "brown"
-    case r <= 50 && g < 50 && b === g:
+    case r <= 50 && g < 50 && b === g && b > 0:
       return "brown"
-    case r === 0 && g === 0 && b === 0:
-      return "black"
-    case r === g && r === b && g === b:
+    case highestValue - smallestValue <= 15:
       return "gray"
     default:
       return "requires manual sorting"
--- src/api/swatches/index.js
+++ src/api/swatches/index.js
@@ -1,6 +1,6 @@
 import express from "express"
 import mongoose from "mongoose"
-import SwatchSchema from "./model.js"
+import SwatchModel from "./model.js"
 import { jwtAuthMiddleware } from "../lib/jwtAuth.js"
 import { AdminOnlyMiddleware } from "../lib/AdminOnly.js"
 import { componentToHex, getCategoryFromRGB } from "../lib/colorSorter.js"
@@ -9,21 +9,29 @@ const adminRouter = express.Router()
 
 adminRouter.post("/swatch", jwtAuthMiddleware, AdminOnlyMiddleware, async (req, res, next) => {
   try {
-    const { r, g, b } = req.body
+    const { r, g, b, name, baseColor } = req.body
     const rgb = [r, g, b]
     console.log("rgb from body", rgb)
-    const hex = "#" + componentToHex(r) + componentToHex(g) + componentToHex(b)
-    console.log("hex color from rgb", hex)
-    const basicCategory = getCategoryFromRGB(rgb)
+    const existingSwatch = await SwatchModel.findOne({ rgbValue: rgb })
 
-    const newColor = new SwatchSchema({
-      rgbValue: rgb,
-      hexValue: hex,
-      baseColor: basicCategory
-    })
+    if (existingSwatch) {
+      res.status(409).send({ message: "Swatch already exists with the same RGB value" })
+    } else {
+      const hex = "#" + componentToHex(r) + componentToHex(g) + componentToHex(b)
+      console.log("hex color from rgb", hex)
 
-    const newColorSaved = await newColor.save()
-    res.status(201).send(newColorSaved)
+      const basicCategory = req.body.baseColor ? req.body.baseColor : getCategoryFromRGB(rgb)
+
+      const newColor = new SwatchModel({
+        rgbValue: rgb,
+        hexValue: hex,
+        baseColor: basicCategory,
+        name: name
+      })
+
+      const newColorSaved = await newColor.save()
+      res.status(201).send(newColorSaved)
+    }
   } catch (error) {
     next(error)
   }
@@ -31,7 +39,7 @@ adminRouter.post("/swatch", jwtAuthMiddleware, AdminOnlyMiddleware, async (req, 
 
 adminRouter.get("/swatches", async (req, res, next) => {
   try {
-    const colors = await SwatchSchema.find({})
+    const colors = await SwatchModel.find({})
     res.send(colors)
   } catch (error) {
     next(error)
